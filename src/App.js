b64bbcc29e145990343627859955beff37ea9315
sbkjarmul <37276167+sbkjarmul@users.noreply.github.com>
2021-06-26 11:00:58 +0200

    feat: add router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Hero from './components/hero'
 import Navbar from './components/navbar'
 import Footer from './components/footer'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Hero />
+      <Router>
+        <Hero />
+      </Router>
       <Footer />
     </div>
   );
