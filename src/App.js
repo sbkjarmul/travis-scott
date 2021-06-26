72fa12ca62af50a73f1200d0f2488465cd834d6c
sbkjarmul <37276167+sbkjarmul@users.noreply.github.com>
2021-06-26 14:02:33 +0200

    add video page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Hero from './components/hero'
 import Album from './components/album'
 import Navbar from './components/navbar'
+import Video from './components/video'
 import Footer from './components/footer'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Hero />
         <Album />
+        <Video />
       </Router>
       <Footer />
     </div>
--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
+import '../App.css'
 
 const Video = () => {
   return (
-    <>
-    </>
+    <div className="video">
+      <div className="video__wrapper">
+        <ReactPlayer 
+          className="video__player"
+          url="https://vimeo.com/178629241"
+          width="100%"
+          height="100%"
+          controls="true"
+        />
+      </div>
+    </div>
   )
 }
 
